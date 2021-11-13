cb277dd2dba8ad7ce84db2f1b668ca85721cf992
saroj <saroj@DESKTOP-2UUOJHH>
2021-11-13 09:50:27 +0530

    My First Commit

---
+++ src/com/home/MathDemo.java
@@ -0,0 +1,8 @@
+package com.home;
+
+public class MathDemo {
+	public int add(int x, int y){
+		return x+y;
+	}
+
+}
---
+++ src/com/home/runner/Runner.java
@@ -0,0 +1,13 @@
+package com.home.runner;
+
+import com.home.MathDemo;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		MathDemo md=new MathDemo();
+		System.out.println("Addion result is : "+md.add(5, 8));
+
+	}
+
+}
